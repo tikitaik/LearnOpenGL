d74f2b4341fd63715818250cdd05dc9494ab6e3c
ed <edthirst@gmail.com>
2025-07-10 01:42:05 +1000

    added some libraries and include dir for mac yo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "include/stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "shaders/shader.hpp"
 
